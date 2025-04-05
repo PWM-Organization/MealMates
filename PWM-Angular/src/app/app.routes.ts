277600df62cddbbd170e18c6cb211867eec9c804
Pablo <pablo.monzon103@alu.ulpgc.es>
2025-04-05 21:42:32 +0100

    Add blog component and update routing; modify header for blog link

--- PWM-Angular/src/app/app.routes.ts
+++ PWM-Angular/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { RegisterComponent } from './register/register.component';
+import { BlogComponent } from './blog/blog.component';
 
 export const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'login', component: LoginComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'blog', component: BlogComponent },
 ];
