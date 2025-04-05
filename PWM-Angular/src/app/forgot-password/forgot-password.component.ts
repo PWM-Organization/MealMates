99389d42e77d48c514f2118c854eb1933a61c770
Pablo <pablo.monzon103@alu.ulpgc.es>
2025-04-05 21:31:39 +0100

    Add forgot password component with template and styles

---
+++ PWM-Angular/src/app/forgot-password/forgot-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-password',
+  standalone: true,
+  imports: [],
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['../login/login.component.css', '../../styles.css']
+})
+export class ForgotPasswordComponent {
+
+}
