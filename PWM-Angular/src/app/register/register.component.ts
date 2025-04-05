ba5cfbe47b54cf584f89a0cb5e749da70c527849
Pablo <pablo.monzon103@alu.ulpgc.es>
2025-04-05 21:29:04 +0100

    Add register component with template and styles

---
+++ PWM-Angular/src/app/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [],
+  templateUrl: './register.component.html',
+  styleUrls: ['../login/login.component.css', '../../styles.css']
+})
+export class RegisterComponent {
+
+}
