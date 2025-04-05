97f4cbc9b977c6cd458d7fe57c863ff7f03ee3a8
Pablo <pablo.monzon103@alu.ulpgc.es>
2025-04-05 19:07:42 +0100

    Refactor header component to toggle user menu based on login status

--- PWM-Angular/src/app/header/header.component.ts
+++ PWM-Angular/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css', '../../styles.css']
 })
 export class HeaderComponent {
-
+  isLoggedIn = true;
 }
