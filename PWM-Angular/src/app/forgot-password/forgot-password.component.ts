7a44bf8f42c4be5324f29bad646ab90fa9e04e91
Pablo <pablo.monzon103@alu.ulpgc.es>
2025-04-05 21:38:01 +0100

    Enhance forgot password and register components with new templates and styles; integrate routing

--- PWM-Angular/src/app/forgot-password/forgot-password.component.ts
+++ PWM-Angular/src/app/forgot-password/forgot-password.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './forgot-password.component.html',
   styleUrls: ['../login/login.component.css', '../../styles.css']
 })
