584210e5506cc5f4c5059d0f33a60d8ce8ed919d
Pablo <pablo.monzon103@alu.ulpgc.es>
2025-04-06 11:23:49 +0100

    Make components standalone for improved modularity

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
+  standalone: true,
   imports: [],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css', '../../styles.css']
