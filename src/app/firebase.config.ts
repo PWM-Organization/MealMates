9f3849cc9feeb4039c92fb700370d1296fce0f6a
ElenaArtiles <morartielen@gmail.com>
2025-04-29 19:10:37 +0100

    Add firebase configuration file

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCvpDJ4-iBYkRCn7zquz889D5lN9hb1E3s",
+  authDomain: "pwm-angular.firebaseapp.com",
+  databaseURL: "https://pwm-angular-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "pwm-angular",
+  storageBucket: "pwm-angular.firebasestorage.app",
+  messagingSenderId: "202980994346",
+  appId: "1:202980994346:web:1674f91abdf8bb422a8253",
+  measurementId: "G-DYGLHE80DX"
+  };
