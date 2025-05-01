a4d081377173ce194d3420842b0a57bba33521a4
Francisco J <franlsym@users.noreply.github.com>
2025-05-01 21:25:46 +0100

    refactor: move index component to pages folder and update imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { IndexComponent } from './index/index.component';
+import { IndexComponent } from './pages/index/index.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { RegisterComponent } from './pages/register/register.component';
--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { BlogRecipesListComponent } from '../components/blog-recipes-list/blog-recipes-list.component';
+import { BlogRecipesListComponent } from '../../components/blog-recipes-list/blog-recipes-list.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [BlogRecipesListComponent, RouterModule],
+  imports: [
+    BlogRecipesListComponent, 
+    RouterModule
+  ],
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css', '../../styles.css']
+  styleUrls: ['./index.component.css']
 })
 export class IndexComponent {
 
